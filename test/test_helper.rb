08fc7afa2eb42ffffb28d0be0efc195cd0ace144
Matt Brictson <matt@mattbrictson.com>
2019-12-19 08:04:23 -0800

    Correct rubocop 0.78 issues
    - Metrics/LineLength -> Layout/LineLength
    - Lint/NonDeterministicRequireOrder

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,4 @@ require "tomo/plugin/sidekiq"
 require "tomo/testing"
 
 require "minitest/autorun"
-Dir[File.expand_path("support/**/*.rb", __dir__)].each { |rb| require(rb) }
+Dir[File.expand_path("support/**/*.rb", __dir__)].sort.each { |rb| require(rb) }
