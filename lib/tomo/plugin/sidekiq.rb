0e9b2512afb2fc22bbb2c8c1909ce3711df0c4e3
Matt Brictson <mattbrictson@users.noreply.github.com>
2020-05-22 20:10:56 -0700

    Reformat Ruby files for line length of 120 (#24)
    The rubocop community has decided to standardize on 120 characters for
    default line length.
    
    https://github.com/rubocop-hq/rubocop/pull/7952
    
    Update rubocop config and reformat ruby files to embrace this
    convention.

--- lib/tomo/plugin/sidekiq.rb
+++ lib/tomo/plugin/sidekiq.rb
@@ -6,10 +6,7 @@ module Tomo::Plugin::Sidekiq
   extend Tomo::PluginDSL
 
   tasks Tomo::Plugin::Sidekiq::Tasks
-
-  # rubocop:disable Layout/LineLength
   defaults sidekiq_systemd_service: "sidekiq_%{application}.service",
            sidekiq_systemd_service_path: ".config/systemd/user/%{sidekiq_systemd_service}",
            sidekiq_systemd_service_template_path: File.expand_path("sidekiq/service.erb", __dir__)
-  # rubocop:enable Layout/LineLength
 end
