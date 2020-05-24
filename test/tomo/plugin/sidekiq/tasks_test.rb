a65f27997b256b3e34ec20982228f747d9dac02e
Matt Brictson <matt@mattbrictson.com>
2020-05-23 18:03:12 -0700

    More minor reformatting

--- test/tomo/plugin/sidekiq/tasks_test.rb
+++ test/tomo/plugin/sidekiq/tasks_test.rb
@@ -2,17 +2,11 @@ require "test_helper"
 
 class Tomo::Plugin::Sidekiq::TasksTest < Minitest::Test
   def setup
-    @tester = Tomo::Testing::MockPluginTester.new(
-      "sidekiq",
-      settings: { application: "example" }
-    )
+    @tester = Tomo::Testing::MockPluginTester.new("sidekiq", settings: { application: "example" })
   end
 
   def test_setup_systemd
-    @tester.mock_script_result(
-      "ls -A1 /var/lib/systemd/linger",
-      stdout: "testing\n"
-    )
+    @tester.mock_script_result("ls -A1 /var/lib/systemd/linger", stdout: "testing\n")
     expected_scripts = [
       "ls -A1 /var/lib/systemd/linger",
       "mkdir -p .config/systemd/user",
@@ -28,10 +22,7 @@ class Tomo::Plugin::Sidekiq::TasksTest < Minitest::Test
   end
 
   def test_setup_systemd_dies_if_linger_is_disabled
-    @tester.mock_script_result(
-      "ls -A1 /var/lib/systemd/linger",
-      stdout: "some_other_user\n"
-    )
+    @tester.mock_script_result("ls -A1 /var/lib/systemd/linger", stdout: "some_other_user\n")
     error = assert_raises(Tomo::Runtime::TaskAbortedError) do
       @tester.run_task("sidekiq:setup_systemd")
     end
@@ -40,43 +31,28 @@ class Tomo::Plugin::Sidekiq::TasksTest < Minitest::Test
 
   def test_reload
     @tester.run_task("sidekiq:reload")
-    assert_equal(
-      "systemctl --user reload sidekiq_example.service",
-      @tester.executed_script
-    )
+    assert_equal("systemctl --user reload sidekiq_example.service", @tester.executed_script)
   end
 
   def test_restart
     @tester.run_task("sidekiq:restart")
-    assert_equal(
-      "systemctl --user restart sidekiq_example.service",
-      @tester.executed_script
-    )
+    assert_equal("systemctl --user restart sidekiq_example.service", @tester.executed_script)
   end
 
   def test_start
     @tester.run_task("sidekiq:start")
-    assert_equal(
-      "systemctl --user start sidekiq_example.service",
-      @tester.executed_script
-    )
+    assert_equal("systemctl --user start sidekiq_example.service", @tester.executed_script)
   end
 
   def test_stop
     @tester.run_task("sidekiq:stop")
-    assert_equal(
-      "systemctl --user stop sidekiq_example.service",
-      @tester.executed_script
-    )
+    assert_equal("systemctl --user stop sidekiq_example.service", @tester.executed_script)
   end
 
   def test_log
     assert_raises(Tomo::Testing::MockedExecError) do
       @tester.run_task("sidekiq:log", "-f")
     end
-    assert_equal(
-      "journalctl -q --user-unit\=sidekiq_example.service -f",
-      @tester.executed_script
-    )
+    assert_equal("journalctl -q --user-unit\=sidekiq_example.service -f", @tester.executed_script)
   end
 end
